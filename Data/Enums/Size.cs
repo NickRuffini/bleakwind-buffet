b7e0ee0f0b258d6303963221aaa0a310aa3447d0
ruffini <ruffini@ksu.edu>
2020-08-27 08:52:55 -0500

    Finished Entrees

--- Data/Enums/Size.cs
+++ Data/Enums/Size.cs
@@ -1,5 +1,6 @@
 ﻿/*
  * Author: Zachery Brunner
+ * Edited By: Nick Ruffini
  * Class name: Size.cs
  * Purpose: Class used to represent sizes through an enumeration
  */
@@ -14,5 +15,18 @@ namespace BleakwindBuffet.Data.Enums
         Small,
         Medium,
         Large
+    }
+
+    /// <summary>
+    /// Provides available soda flavors!
+    /// </summary>
+    public enum SodaFlavor
+    {
+        Blackberry,
+        Cherry,
+        Grapefruit,
+        Lemon,
+        Peach,
+        Watermelon
     }
 }
