e91e3a56da828e7bf49fcb1cbf4d066aa165ab74
Nick Ruffini <ruffini@users.campus>
2020-09-10 14:19:28 -0500

    Working on Menu testing

---
+++ DataTests/UnitTests/MenuTests.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Author: Nick Ruffini
+ * Class: MenuTests.cs
+ * Purpose: Test the Menu.cs class in the Data library
+ */
+
+using Xunit;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Entrees;
+
+namespace BleakwindBuffet.DataTests.UnitTests
+{
+    public class MenuTests
+    {
+        [Fact]
+        public void PopulatesEntreesCorrectly()
+        {
+            IEnumerable<IOrderItem> test = Menu.Entrees();
+
+
+            Assert.Collection<IOrderItem>(test,
+                item => Assert.IsType(BriarheartBurger, item), 
+                item => Assert.Equal("Double Draugr", item.ToString()),
+                item => Assert.Equal("Garden Orc Omelette", item.ToString()),
+                item => Assert.Equal("Philly Poacher", item.ToString()),
+                item => Assert.Equal("Smokehouse Skeleton", item.ToString()),
+                item => Assert.Equal("Thalmor Triple", item.ToString()),
+                item => Assert.Equal("Thugs T-Bone", item.ToString())
+                );
+        }
+        [Fact]
+
+    }
+}
